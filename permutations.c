80809e67523d742e077ddd222fc525c4cf8c29c7
Imran Khan <imran1524@gmail.com>
2024-05-04 07:13:01 +0100

    Removed inverse permutation

--- permutations.c
+++ permutations.c
@@ -77,58 +77,5 @@ void forward_permutation(ladybug_state_t *s) {
 //    printf("State after applying forward transform:\n");
 //    print_state(s);  // Correctly passing 's'
 //    printf("\n");
-
-
 }
 
-//INVERSE PERMUTATION
-//TODO: Add round constant as input parameter
-void inverse_permutation(ladybug_state_t *s){
-//    printf("INVERSE PERMUTATION:\n");
-//    printf("State before applying inverse S-box\n");
-//    print_state(s);
-
-    uint8_t bundles[64] = {0};
-    // Creating 64 5-bit bundles from 5 64-bit blocks
-    for(size_t i = 0; i < 64; i++) {
-        for(size_t j = 0; j < BLOCK_NUMBER; j++) {
-            bundles[i] |= ((s->x[j] >> (63-i)) & 0x1) << (BLOCK_NUMBER - 1 - j);
-        }
-    }
-//    printf("Before applying inverse 5-bit S-box:\n");
-//    print_bundles(bundles, 64);
-
-    // Apply Inverse S-Box
-    for(size_t i = 0; i < 64; i++) {
-          bundles[i] = inverse_s_box[bundles[i]]; // Assuming forward_s_box is correctly defined
-    }
-
-//    printf("\n");
-//    printf("After applying inverse 5-bit S-box:\n");
-//    print_bundles(bundles, 64);
-    //Reassembling 5 64-bit blocks from 64 5-bit bundles
-
-    create_blocks_from_bundles(bundles, s);
-//    printf("\n");
-//    printf("State before applying inverse transform:\n");
-//    print_state(s);  // Correctly passing 's'
-//    printf("\n");
-
-    // INVERSE TRANSFORM
-    // TRANSPOSE ONMNT MATRIX
-    uint8_t transposedONMNT[BLOCK_SIZE][BLOCK_SIZE];
-//    transpose_ONMNT(ONMNT, transposedONMNT);
-
-    inverse_transform(s, NMNT);  // Directly passing 's' since it's a pointer
-//    inverse_transform(s, ONMNT, ROUNDS);  // Directly passing 's' since it's a pointer
-//    printf("State after applying inverse transform:\n");
-//    print_state(s);  // Correctly passing 's'
-//    printf("\n");
-
-    char retrievedText[41]; // Enough for 40 characters and a null terminator
-    blocks_to_text(s, retrievedText, sizeof(retrievedText));  // Assuming blocks_to_text is updated to take a pointer
-//    printf("Recovered plaintext:\n");
-//    for(uint8_t i = 0; i < 40; i++){
-//        printf("%c", retrievedText[i]);
-//    }
-}
--- permutations.h
+++ permutations.h
@@ -9,12 +9,9 @@
 #include "config.h"
 
 void FP1 (ladybug_state_t *s);
-void IP1 (ladybug_state_t *s);
 
 void forward_permutation(ladybug_state_t *s);
-void inverse_permutation(ladybug_state_t *s);
 
 extern uint8_t forward_s_box[1 << SBOX_SIZE];
-extern uint8_t inverse_s_box[1 << SBOX_SIZE];
 
 #endif //DIFFUSION_LAYER_PERMUTATIONS_H
